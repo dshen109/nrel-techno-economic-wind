787cc91feecd0ee6d7dd9a74fb827c86749d436f
Daniel Shen <dshen109@gmail.com>
2022-02-19 00:22:03 -0500

    play around with init

--- wind_ensemble/__init__.py
+++ wind_ensemble/__init__.py
@@ -1,11 +1,11 @@
 import os
-pywtk_cache = os.path.join(
-    os.environ['HOME'], 'dropbox', 'pywtk-data')
+# Late setting of env so pywtk loads correctly
+pywtk_cache = os.path.join(os.environ['HOME'], 'dropbox', 'pywtk-data')
 os.environ['PYWTK_CACHE_DIR'] = pywtk_cache
 
 import pandas
-from pywtk.wtk_api import get_nc_data, site_from_cache, \
-    WIND_FCST_DIR, WIND_MET_NC_DIR
+from pywtk.site_lookup import get_3tiersites_from_wkt
+from pywtk.wtk_api import get_nc_data, WIND_FCST_DIR, WIND_MET_NC_DIR
 
 
 if __name__ == '__main__':
@@ -34,4 +34,7 @@ if __name__ == '__main__':
     fcst_attributes = attributes_hour + attributes_4_hour + attributes_6_hour + attributes_day
     fcst_data = get_nc_data(
         site_id, start, end, fcst_attributes, utc=utc, nc_dir=WIND_FCST_DIR)
+
+    # Longitude first
+    sites = get_3tiersites_from_wkt('POINT(23.8000 -68.33000)')
     assert 0
