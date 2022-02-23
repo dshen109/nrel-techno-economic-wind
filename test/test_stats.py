4a241099eadd6e407e664cc0e8bd121aaced4072
Daniel Shen <dshen109@gmail.com>
2022-02-22 19:01:33 -0500

    test a fit that dont do so good

--- test/test_stats.py
+++ test/test_stats.py
@@ -23,3 +23,13 @@ class TestHyperbolic(TestCase):
         self.assertAlmostEqual(hyperbolic.cdf(-1), 0.1, places=2)
         self.assertAlmostEqual(hyperbolic.cdf(4), 0.9, places=2)
         self.assertAlmostEqual(hyperbolic.stats('k'), kurtosis, places=2)
+
+    def test_another_fit(self):
+        mean = 0.010283576
+        percentiles = {0.1: 0.0, 0.9: 0.1240082}
+        kurtosis = 18
+        hyperbolic, _ = stats.fit_hyperbolic(
+            mean, percentiles, kurtosis, percentile_weight=100)
+        self.assertAlmostEqual(hyperbolic.cdf(0), 0.1, places=2)
+        self.assertAlmostEqual(hyperbolic.cdf(0.12401), 0.9, places=2)
+        self.assertAlmostEqual(hyperbolic.stats('k'), kurtosis, places=2)
